3f738b5d0bd86c623a91499607aaeb14a751614a
lixiang <2646304485@qq.com>
2020-09-10 20:20:23 +0800

    最后一次提交

---
+++ src/views/props+$emit/Child.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>孩子</h1>
+        <p>{{message}}</p>
+        <button @click="handleClick">按钮</button>
+    </div>
+</template>
+<script>
+import Child from './Child';
+export default {
+    props:['message'], 
+
+    methods: {
+        handleClick(){
+            this.$emit('changeMessage','Bay');
+        }
+    },
+}
+</script>
+<style scoped>
+
+</style>>
---
+++ src/views/props+$emit/Parent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>父亲</h1>
+        <p>{{message}}</p>
+        <Child :message="message" @changeMessage="message=$event"/>
+    </div>
+</template>
+<script>
+import Child from './Child';
+export default {
+    data() {
+        return {
+            message:"hello 加油"     
+        }
+    },
+    components:{
+        Child
+    }
+}
+</script>
+<style scoped>
+
+</style>>
